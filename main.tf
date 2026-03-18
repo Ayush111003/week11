ef030904bd192eb9e00715f9141a31ba30484a81
Ayush111003 <126341508+Ayush111003@users.noreply.github.com>
2026-03-18 15:27:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ resource "aws_security_group" "example" {
   name        = "example"
   description = "example security group"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ ingress {
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/16"]
+}
 }
