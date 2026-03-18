2981d160b44a51cc8a4538fc3e1b7a2d3d9c5024
Ayush111003 <126341508+Ayush111003@users.noreply.github.com>
2026-03-18 15:12:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "us-west-2"
-}
-
 resource "aws_security_group" "example" {
   name        = "example"
   description = "example security group"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
